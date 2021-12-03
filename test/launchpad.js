92e2c2ed7f45828c4a8d223f31f3c43c038e6fe8
wow64bb <bushimushi22@gmail.com>
2021-12-03 12:36:00 +0800

    refactor test file into single file

--- test/launchpad.js
+++ test/launchpad.js
@@ -1,4 +1,5 @@
 const Launchpad = artifacts.require("LaunchPad");
+const MyToken = artifacts.require("MyToken");
 
 /*
  * uncomment accounts to access the test accounts made available by the
@@ -6,8 +7,23 @@ const Launchpad = artifacts.require("LaunchPad");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("Launchpad", function( /* accounts */ ) {
-    it("should assert true", async function() {
+    it("should deploy contract", async function() {
         await Launchpad.deployed();
         return assert.isTrue(true);
+    });
+});
+
+contract("MyToken", (accounts) => {
+    it("should deploy contract", async function() {
+        await MyToken.deployed();
+        return assert.isTrue(true);
+    });
+
+    it("should return the list of accounts", async() => {
+        console.log(accounts);
+    });
+
+    it("should able to give allowance", async() => {
+        console.log(accounts);
     });
 });
