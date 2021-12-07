3326b28b85550cc7c2e72673b25f5529b579eed1
yuanjie0108 <95063321+yuanjie0108@users.noreply.github.com>
2021-12-07 16:40:02 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+App = {
+    web3Provider: null,
+    contracts: {},
+  
+    init: async function() {
+      // Load json
+      //return await App.initWeb3();
+    },
+  
+    initWeb3: async function() {
+      
+      if (window.ethereum) {
+        App.web3Provider = window.ethereum;
+        try {
+          // Request account access
+          await window.ethereum.request({ method: "eth_requestAccounts" });;
+        } catch (error) {
+          // User denied account access...
+          console.error("User denied account access")
+        }
+      }
+      // Legacy dapp browsers...
+      else if (window.web3) {
+  
+        App.web3Provider = window.web3.currentProvider;
+      }
+      // If no injected web3 instance is detected, fall back to Ganache
+      else {
+        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      }
+      web3 = new Web3(App.web3Provider);
+  
+      return App.initContract();
+    }
+  };
+  
