312f41139f28bead96e705bb62dfc7d1fb235f41
wow64bb <bushimushi22@gmail.com>
2021-12-10 14:59:54 +0800

    WIP: frontend retrieve info from launchpad contract

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 /*
  * http://trufflesuite.com/tutorial/#creating-a-user-interface-to-interact-with-the-smart-contract
+ * https://ethereum.org/en/developers/tutorials/calling-a-smart-contract-from-javascript/
  */
+
 
 App = {
     web3Provider: null,
@@ -8,7 +10,7 @@ App = {
 
     init: async function() {
         // Load pets.
-        $.getJSON('../launchpad.json', function(data) {
+        $.getJSON('../data.json', function(data) {
             var load = $('#load');
             var template = $('#template');
 
@@ -61,10 +63,21 @@ App = {
 
             // Set the LaunchPad for our contract
             App.contracts.LaunchPad.setProvider(App.web3Provider);
-            console.log(data)
+            //console.log(data)
 
             return App.showResult();
+            //return App.showLaunchPads();
         });
+
+        $.getJSON('MyToken.json', function(data) {
+            var MyTokenArtifact = data;
+            App.contracts.MyToken = TruffleContract(MyTokenArtifact);
+            App.contracts.MyToken.setProvider(App.web3Provider);
+            //console.log(data)
+            return App.dummyToken();
+        });
+
+        //wtf is this?
         //return App.bindEvents();
     },
 
@@ -86,13 +99,110 @@ App = {
             }).then(function(result) {
 
                 console.log("Owner of launchpad contract: " + result)
+                return App.displayLaunchPads();
 
             }).catch(function(err) {
                 console.log("Unable to connect contract, is the local node working? Are the contracts deployed?")
                     //console.log(err.message);
             });
         });
-    }
+    },
+
+    dummyToken: function() {
+        //this function is for testing a simple token in launchpad, remove in production
+        var tokenInstance;
+        web3.eth.getAccounts(function(error, accounts) {
+            if (error) {
+                console.log(error);
+            }
+
+            App.contracts.MyToken.deployed().then(function(instance) {
+
+                tokenInstance = instance;
+
+                // retrieve owner of contract
+                return tokenInstance.admin();
+
+            }).then(async function(result) {
+                //retrieve contract deployer address
+                console.log("Admin of MyToken contract: " + result)
+
+                //retrieve user balance
+                const balance = await tokenInstance.balanceOf(result)
+                console.log("User balance: " + balance)
+
+                //retrieve launchpad contract address
+                var launchpad = await App.contracts.LaunchPad.deployed()
+                console.log("LaunchPad contract address: " + launchpad.address)
+
+                //retrieve allowance before increasing it
+                console.log(await tokenInstance)
+                    //await tokenInstance.allowance(result, launchpad, { from: result })
+                    //console.log("Current allowance: " + value)
+
+                //handle allowance
+                //await tokenInstance.increaseAllowance(launchpad, 1000)
+
+            }).catch(function(err) {
+                console.error(err.message);
+            });
+        });
+    },
+
+    displayLaunchPads: function() {
+        var launchPadInstance;
+        web3.eth.getAccounts(function(error, accounts) {
+            if (error) {
+                console.log(error);
+            }
+            console.log("Account address: " + accounts)
+
+            App.contracts.LaunchPad.deployed().then(function(instance) {
+
+                launchPadInstance = instance;
+
+                // find how many launchpads are available
+                return launchPadInstance.totalLaunchpads({ from: accounts });
+
+            }).then(function(result) {
+
+                console.log("Total launchpads: " + result)
+
+                //clear screen by default since it's load from JSON file
+                var load = $('#load');
+                var template = $('#template');
+                load.empty();
+
+                //redeclare variable
+                var totalLaunchPads = result;
+
+                if (totalLaunchPads == 0) {
+
+                    console.log("no launchpad detected!")
+
+                } else {
+
+                    console.log("Launchpads detected")
+
+                    //prepare variables to load them via loop
+                    var launchpad_info;
+
+                    for (i = 0; i < totalLaunchPads.length; i++) {
+                        launchpad_info = launchPadInstance.launchpads.call(i);
+                        //take all from here https://github.com/wow32/Blockchain-Assignment/blob/main/contracts/LaunchPad.sol#L15-L25
+                        console.log(launchpad_info.sender) //example of retrieving sender
+                        template.find('.price-tag').text(launchpad_info.pricePerToken);
+
+                        //append info into UI
+                        load.append(template.html());
+                    }
+                }
+
+            }).catch(function(err) {
+                console.error("Error: " + err.message);
+            });
+        });
+    },
 
     //   bindEvents: function() {
     //     $(document).on('click', '.btn-adopt', App.handleAdopt);
